bfb77c277102968a07f6c47a7094325e46445ccb
bogdanbujor99 <79144278+bogdanbujor99@users.noreply.github.com>
2021-09-16 20:13:39 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,33 +1,69 @@
 #include <iostream>
-#include "Vector.h"
+#include <string>
+#include <map>
+#include <queue>
+using namespace std;
 
+class Compare {
+public:
+    bool operator()(pair<string,int>p1, pair<string,int> p2) const {
+        if (p1.second < p2.second)return 1;
+        else if (p1.second == p2.second) {
+            if (p1.first> p2.first)return 1;
+        }
+        return 0;
+    }
+};
+ 
 int main()
 {
-	Vector<int, double> v;
-	int n;
-	double a;
-	cout << "Dati numarul de elemente: ";
-	cin >> n;
-	cout << "Vectorul este: ";
-	for (int i = 1; i <= n; i++) {
-		cin >> a;
-		v.Push(a);
-	}
-	v.Detele(2);
-	cout << "Dupa stergerea elementului de pe pozitia 2 ";
-	v.Print();
-	cout << endl;
-	v.Insert(3, 4);
-	cout << "Dupa inserarea elementului 4 pe pozitia 3 ";
-	v.Print();
-	cout << endl;
-	v.Set(2,5);
-	cout << "Dupa setarea elementului 5 pe pozitia 2 ";
-	v.Print();
-	cout << endl;
-	v.Sort(compara);
-	cout << "Dupa sortare ";
-	v.Print();
-	cout << endl;
-	v.FirstIndex(equal,3);
+    map<string, int>CUVINTE;
+    string s;
+    getline(cin, s);
+    string cuvant;
+    for (string::iterator it = s.begin(); it != s.end(); ++it) {
+        if (*it == ' ') {
+            if(cuvant!="")
+            CUVINTE[cuvant]++;
+            string a;
+            cuvant = a;
+        }
+        else  if (*it == ','){
+            if (cuvant != "")
+            CUVINTE[cuvant]++;
+            string a;
+            cuvant = a;
+        }
+        else if (*it == '.') {
+            if (cuvant != "")
+            CUVINTE[cuvant]++;
+            string a;
+            cuvant = a;
+        }
+        else  if (*it == '!') {
+            if (cuvant != "")
+            CUVINTE[cuvant]++;
+            string a;
+            cuvant = a;
+        }
+        else if (*it == '?') {
+            if (cuvant != "")
+            CUVINTE[cuvant]++;
+            string a;
+            cuvant = a;
+        }
+        else {
+            if (*it >= 'A' && *it <= 'Z')*it = *it + 32;
+            cuvant.push_back(*it);
+        }
+    }
+    pair <string, int>c;
+    priority_queue < pair<string,int>, vector<pair<string, int>> , Compare> Priority;
+
+    for (map<string, int>::iterator it = CUVINTE.begin(); it != CUVINTE.end(); it++) {
+        pair<string, int>c;
+        c.first = it->first;
+        c.second = it->second;
+        Priority.push(c);
+    }
 }
